575cb46c8f16dba46e505b777428cc0dae1d6084
weppyk <radek.drlik@gmail.com>
2013-08-28 20:03:40 +0000

    Start programming

---
+++ sms.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+var number= +420722118864;
+var message = "Hello word to your mobile";
+
+var smssend= function(number,message) {
+    console.log(number,message);
+}
+smssend(number,message);
